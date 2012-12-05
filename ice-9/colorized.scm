de6502f45b9567a41ce0d8ef853d8506fcbd2ea3
Nala Ginrut <nalaginrut@gmail.com>
2012-12-05 16:52:58 +0800

    Fixed: the license declaration.

--- ice-9/colorized.scm
+++ ice-9/colorized.scm
@@ -1,11 +1,11 @@
 ;;  Copyright (C) 2012
 ;;      "Mu Lei" known as "NalaGinrut" <NalaGinrut@gmail.com>
-;;  Ragnarok is free software: you can redistribute it and/or modify
+;;  Guile-colorized is free software: you can redistribute it and/or modify
 ;;  it under the terms of the GNU General Public License as published by
 ;;  the Free Software Foundation, either version 3 of the License, or
 ;;  (at your option) any later version.
 
-;;  Ragnarok is distributed in the hope that it will be useful,
+;;  Guile-colorized is distributed in the hope that it will be useful,
 ;;  but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;;  GNU General Public License for more details.
